b920aa366610d8b3e33a464b58f207e5b711f08d
Mark Grimes <mark.grimes@cern.ch>
2014-04-09 15:21:15 +0200

    Added unit tests to load menus from different formats and check they give the same results (currently fails becuase the method to load old format files has not been written)

---
+++ test/unitTestSuites/TriggerMenuUnitTestSuite.cpp
@@ -0,0 +1,151 @@
+#include <cppunit/extensions/HelperMacros.h>
+
+//
+// Forward declarations
+//
+namespace l1menu
+{
+	class TriggerMenu;
+}
+
+/** @brief A cppunit TestFixture to test loading and saving menus and results to a file.
+ *
+ * @author Mark Grimes (mark.grimes@bristol.ac.uk)
+ * @date 08/Apr/2014
+ */
+class TriggerMenuUnitTestSuite : public CPPUNIT_NS::TestFixture
+{
+	CPPUNIT_TEST_SUITE(TriggerMenuUnitTestSuite);
+	CPPUNIT_TEST(testFormatsAreEqual);
+	CPPUNIT_TEST(testFormatsGiveSameResult);
+	CPPUNIT_TEST_SUITE_END();
+
+protected:
+
+public:
+	void setUp();
+
+protected:
+	void testFormatsAreEqual();
+	void testFormatsGiveSameResult();
+
+	// These are set in the setUp() method
+	std::unique_ptr<l1menu::TriggerMenu> pMenuFromXMLFormat_;
+	std::unique_ptr<l1menu::TriggerMenu> pMenuFromOldFormat_;
+};
+
+
+
+
+
+#include <cppunit/config/SourcePrefix.h>
+#include <stdexcept>
+
+#include "TestParameters.h"
+#include "l1menu/IL1MenuFile.h"
+#include "l1menu/TriggerMenu.h"
+#include "l1menu/ITrigger.h"
+#include "l1menu/ICachedTrigger.h"
+#include "l1menu/ISample.h"
+#include "l1menu/IEvent.h"
+#include "l1menu/tools/fileIO.h"
+
+CPPUNIT_TEST_SUITE_REGISTRATION(TriggerMenuUnitTestSuite);
+
+void TriggerMenuUnitTestSuite::setUp()
+{
+	std::string inputXMLFilename=TestParameters<std::string>::instance().getParameter( "TEST_XMLMENU_FILENAME" );
+	std::string inputOLDFilename=TestParameters<std::string>::instance().getParameter( "TEST_MENU_FILENAME" );
+	std::unique_ptr<l1menu::IL1MenuFile> pXmlMenus;
+	std::unique_ptr<l1menu::IL1MenuFile> pOldMenus;
+	CPPUNIT_ASSERT_NO_THROW( pXmlMenus=l1menu::IL1MenuFile::getInputFile( l1menu::IL1MenuFile::FileFormat::XML, inputXMLFilename ) );
+	CPPUNIT_ASSERT_NO_THROW( pOldMenus=l1menu::IL1MenuFile::getInputFile( l1menu::IL1MenuFile::FileFormat::OLD, inputOLDFilename ) );
+
+	std::vector< std::unique_ptr<l1menu::TriggerMenu> > menusFromXML=pXmlMenus->getMenus();
+	std::vector< std::unique_ptr<l1menu::TriggerMenu> > menusFromOLD=pOldMenus->getMenus();
+
+	CPPUNIT_ASSERT_EQUAL( menusFromXML.size(), menusFromOLD.size() );
+	CPPUNIT_ASSERT( menusFromXML.size()>0 );
+
+	pMenuFromXMLFormat_=std::move( menusFromXML.front() );
+	pMenuFromOldFormat_=std::move( menusFromOLD.front() );
+
+	CPPUNIT_ASSERT( pMenuFromXMLFormat_!=nullptr );
+	CPPUNIT_ASSERT( pMenuFromOldFormat_!=nullptr );
+}
+
+void TriggerMenuUnitTestSuite::testFormatsAreEqual()
+{
+	//
+	// Now that I have two menus (one from each of the file formats) I can run some checks to
+	// make sure they both have the same values.
+	//
+	CPPUNIT_ASSERT_EQUAL( pMenuFromXMLFormat_->numberOfTriggers(), pMenuFromOldFormat_->numberOfTriggers() );
+
+	// Loop over all of the triggers and check each one
+	for( size_t triggerIndex=0; triggerIndex<pMenuFromXMLFormat_->numberOfTriggers(); ++triggerIndex )
+	{
+		l1menu::ITrigger& xmlTrigger=pMenuFromXMLFormat_->getTrigger( triggerIndex );
+		l1menu::ITrigger& oldTrigger=pMenuFromOldFormat_->getTrigger( triggerIndex );
+
+		CPPUNIT_ASSERT_EQUAL( xmlTrigger.name(), oldTrigger.name() );
+		CPPUNIT_ASSERT_EQUAL( xmlTrigger.version(), oldTrigger.version() );
+		CPPUNIT_ASSERT_EQUAL( xmlTrigger.thresholdsAreCorrelated(), oldTrigger.thresholdsAreCorrelated() );
+
+		std::vector<std::string> xmlParameterNames=xmlTrigger.parameterNames();
+		std::vector<std::string> oldParameterNames=oldTrigger.parameterNames();
+
+		CPPUNIT_ASSERT_EQUAL( xmlParameterNames.size(), oldParameterNames.size() );
+		for( size_t parameterIndex=0; parameterIndex<xmlParameterNames.size(); ++parameterIndex )
+		{
+			CPPUNIT_ASSERT_EQUAL( xmlParameterNames[parameterIndex], oldParameterNames[parameterIndex] );
+			CPPUNIT_ASSERT_EQUAL( xmlTrigger.parameter( xmlParameterNames[parameterIndex] ), oldTrigger.parameter( oldParameterNames[parameterIndex] ) );
+		} // end of loop over parameters
+	} // end of loop over triggers
+}
+
+void TriggerMenuUnitTestSuite::testFormatsGiveSameResult()
+{
+	//
+	// Try running both menus over the same sample and make sure they
+	// have the same results.
+	//
+	std::string sampleFilename=TestParameters<std::string>::instance().getParameter( "TEST_SAMPLE_FILENAME" );
+	std::unique_ptr<l1menu::ISample> pSample;
+	CPPUNIT_ASSERT_NO_THROW( pSample=l1menu::tools::loadSample( sampleFilename ) );
+
+	//
+	// First I need to create a series of cached triggers. These speed up running massively
+	// because it cuts out the expensive string comparison when seeing if a trigger passes
+	// an event.
+	//
+	std::vector< std::unique_ptr<l1menu::ICachedTrigger> > cachedTriggersFromXMLFormat;
+	for( size_t index=0; index<pMenuFromXMLFormat_->numberOfTriggers(); ++index )
+	{
+		std::unique_ptr<l1menu::ICachedTrigger> newTrigger=pSample->createCachedTrigger(pMenuFromXMLFormat_->getTrigger(index));
+		cachedTriggersFromXMLFormat.push_back( std::move(newTrigger) );
+	}
+
+	std::vector< std::unique_ptr<l1menu::ICachedTrigger> > cachedTriggersFromOldFormat;
+	for( size_t index=0; index<pMenuFromOldFormat_->numberOfTriggers(); ++index )
+	{
+		std::unique_ptr<l1menu::ICachedTrigger> newTrigger=pSample->createCachedTrigger(pMenuFromOldFormat_->getTrigger(index));
+		cachedTriggersFromOldFormat.push_back( std::move(newTrigger) );
+	}
+
+	CPPUNIT_ASSERT_EQUAL( cachedTriggersFromXMLFormat.size(), cachedTriggersFromOldFormat.size() );
+
+	// Loop over all the events
+	for( size_t eventNumber=0; eventNumber<pSample->numberOfEvents(); ++eventNumber )
+	{
+		const l1menu::IEvent& event=pSample->getEvent( eventNumber );
+
+		// Loop over all of the triggers and check each one gives the same result
+		for( size_t triggerIndex=0; triggerIndex<cachedTriggersFromXMLFormat.size(); ++triggerIndex )
+		{
+			std::unique_ptr<l1menu::ICachedTrigger>& pXmlTrigger=cachedTriggersFromXMLFormat[triggerIndex];
+			std::unique_ptr<l1menu::ICachedTrigger>& pOldTrigger=cachedTriggersFromOldFormat[triggerIndex];
+			CPPUNIT_ASSERT_EQUAL_MESSAGE( "Trigger gives a different result for event "+std::to_string(eventNumber), pXmlTrigger->apply(event), pOldTrigger->apply(event) );
+		}
+	}
+}
