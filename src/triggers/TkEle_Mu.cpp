7e8a3be5c9ed8c0f6dd1703f7a65520e1ef3efc4
Brian Winer <brianwiner@gmail.com>
2014-04-15 16:26:57 -0400

    fix binning and add v1

--- src/triggers/TkEle_Mu.cpp
+++ src/triggers/TkEle_Mu.cpp
@@ -11,8 +11,8 @@ namespace l1menu
 		/** @brief Cross trigger of the SingleJetCentral and SingleMuEta triggers.
 		 *
 		 * Combines the following triggers: <br/>
-		 * L1_SingleIsoEG version 0 <br/>
-		 * L1_SingleIsoMu version 0 <br/>
+		 * L1_SingleTkEle version 0 <br/>
+		 * L1_SingleMu version 0 <br/>
 		 *
 		 * @author Individual triggers coded by Brian Winer, re-factored into a derived class of
 		 * CrossTrigger by Mark Grimes (mark.grimes@bristol.ac.uk).
@@ -26,6 +26,21 @@ namespace l1menu
 			virtual unsigned int version() const;
 		}; // end of version 0 class
 
+                
+		/*
+		 * Combines the following triggers: <br/>
+		 * L1_SingleTkEG version 1 which used second TkEle Collection with lower Pt cut <br/>
+		 * L1_SingleMu version 0 <br/>
+		*/
+		class TkEle_Mu_v1 : public l1menu::triggers::CrossTrigger
+		{
+		public:
+			TkEle_Mu_v1();
+			virtual const std::string name() const;
+			virtual unsigned int version() const;
+		}; // end of version 1 class
+
+
 
 		/* The REGISTER_TRIGGER macro will make sure that the given trigger is registered in the
 		 * l1menu::TriggerTable when the program starts. I also want to provide some suggested binning
@@ -34,15 +49,16 @@ namespace l1menu
 		 * at program startup. The function takes no parameters and returns void. In this case I'm
 		 * giving it a lambda function.
 		 */
-		REGISTER_TRIGGER_AND_CUSTOMISE( TkEle_Mu_v0,
+		REGISTER_TRIGGER_AND_CUSTOMISE( TkEle_Mu_v1,
 			[]() // Use a lambda function to customise rather than creating a named function that never gets used again.
 			{
 				l1menu::TriggerTable& triggerTable=l1menu::TriggerTable::instance();
-				TkEle_Mu_v0 tempTriggerInstance;
+				TkEle_Mu_v1 tempTriggerInstance;
 				triggerTable.registerSuggestedBinning( tempTriggerInstance.name(), "leg1threshold1", 100, 0, 100 );
 				triggerTable.registerSuggestedBinning( tempTriggerInstance.name(), "leg2threshold1", 100, 0, 100 );
 			} // End of customisation lambda function
 		) // End of REGISTER_TRIGGER_AND_CUSTOMISE macro call
+		REGISTER_TRIGGER( TkEle_Mu_v0 )
 
 
 	} // end of namespace triggers
@@ -64,4 +80,20 @@ const std::string l1menu::triggers::TkEle_Mu_v0::name() const
 unsigned int l1menu::triggers::TkEle_Mu_v0::version() const
 {
 	return 0;
+}
+
+l1menu::triggers::TkEle_Mu_v1::TkEle_Mu_v1()
+	: CrossTrigger( new l1menu::triggers::SingleTkEleEta_v1, new l1menu::triggers::SingleMuEta_v0 )
+{
+	// No operation besides passing the sub-triggers onto the base class
+}
+
+const std::string l1menu::triggers::TkEle_Mu_v1::name() const
+{
+	return "L1_TkEle_Mu";
+}
+
+unsigned int l1menu::triggers::TkEle_Mu_v1::version() const
+{
+	return 1;
 }
