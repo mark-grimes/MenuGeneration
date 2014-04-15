c0354c342ed45629efe96dfeeaf9453dd8aeb4fa
Brian Winer <brianwiner@gmail.com>
2014-04-15 16:26:57 -0400

    fix binning and add v1

--- src/triggers/SingleTkEleEta.cpp
+++ src/triggers/SingleTkEleEta.cpp
@@ -18,14 +18,15 @@ namespace l1menu
 		 * at program startup. The function takes no parameters and returns void. In this case I'm
 		 * giving it a lambda function.
 		 */
-		REGISTER_TRIGGER_AND_CUSTOMISE( SingleTkEleEta_v0,
+		REGISTER_TRIGGER_AND_CUSTOMISE( SingleTkEleEta_v1,
 			[]() // Use a lambda function to customise rather than creating a named function that never gets used again.
 			{
 				l1menu::TriggerTable& triggerTable=l1menu::TriggerTable::instance();
-				SingleTkEleEta_v0 tempTriggerInstance;
-				triggerTable.registerSuggestedBinning( tempTriggerInstance.name(), "threshold1", 100, 0, 60 );
+				SingleTkEleEta_v1 tempTriggerInstance;
+				triggerTable.registerSuggestedBinning( tempTriggerInstance.name(), "threshold1", 100, 0, 100 );
 			} // End of customisation lambda function
 		) // End of REGISTER_TRIGGER_AND_CUSTOMISE macro call
+		REGISTER_TRIGGER( SingleTkEleEta_v0 )
 
 
 	} // end of namespace triggers
@@ -73,6 +74,39 @@ bool l1menu::triggers::SingleTkEleEta_v0::thresholdsAreCorrelated() const
 unsigned int l1menu::triggers::SingleTkEleEta_v0::version() const
 {
 	return 0;
+}
+
+bool l1menu::triggers::SingleTkEleEta_v1::apply( const l1menu::L1TriggerDPGEvent& event ) const
+{
+	const L1Analysis::L1AnalysisDataFormat& analysisDataFormat=event.rawEvent();
+	const bool* PhysicsBits=event.physicsBits();
+
+	bool raw = PhysicsBits[0];   // ZeroBias
+	if (! raw) return false;
+
+	bool ok=false;
+	int NTkele = analysisDataFormat.NTkele2;
+	for (int ue=0; ue < NTkele; ue++) {
+		int bx = analysisDataFormat.BxTkel2[ue];
+		if (bx != 0) continue;
+		float eta = analysisDataFormat.EtaTkel2[ue];
+		if (eta < regionCut_ || eta > 21.-regionCut_) continue;  // eta = 5 - 16
+		float rank = analysisDataFormat.EtTkel2[ue];    // the rank of the electron
+		float pt = rank ;
+		if (pt >= threshold1_) ok = true;
+	}  // end loop over EM objects
+
+	return ok;
+}
+
+bool l1menu::triggers::SingleTkEleEta_v1::thresholdsAreCorrelated() const
+{
+	return false;
+}
+
+unsigned int l1menu::triggers::SingleTkEleEta_v1::version() const
+{
+	return 1;
 }
 
 l1menu::triggers::SingleTkEleEta::SingleTkEleEta()
